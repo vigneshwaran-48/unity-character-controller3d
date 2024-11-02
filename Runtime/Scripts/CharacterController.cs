f93ecf2333654624820d9b6c66071f6f0a07568c
vigneshwaran48 <p3487260@gmail.com>
2024-11-02 12:46:49 +0530

    Added run movement and its animation too

--- Runtime/Scripts/CharacterController.cs
+++ Runtime/Scripts/CharacterController.cs
@@ -2,20 +2,23 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace charactercontroller {
-
+namespace charactercontroller
+{
     [RequireComponent(typeof(Rigidbody), typeof(PlayerInput))]
-    public class CharacterController : MonoBehaviour {
-
+    public class CharacterController : MonoBehaviour
+    {
         [Header("Movement Settings")]
-        [Tooltip("Movement speed of the character")]
+        [Tooltip("Walk speed of the character")]
         [SerializeField]
-        private float _speed;
+        private float _walkSpeed = 5f;
+
+        [Tooltip("Running speed of the character")]
+        [SerializeField]
+        private float _runSpeed = 10f;
 
         [Tooltip("Rotation speed to the character")]
         [SerializeField]
         private float _rotationSpeed;
-
 
         [Tooltip("This will be used for rotation")]
         [SerializeField]
@@ -34,68 +37,110 @@ namespace charactercontroller {
         [SerializeField]
         private LayerMask _groundLayer;
 
+        // Components
         private Rigidbody _rigidbody;
         private PlayerInput _playerInput;
         private Transform _cameraTransform;
-        private bool _isGrounded;
         private Animator _animator;
 
-        void Awake() {
+        private bool _isGrounded;
+        private bool _isRunning;
+
+        void Awake()
+        {
             _rigidbody = GetComponent<Rigidbody>();
             _playerInput = GetComponent<PlayerInput>();
             _animator = GetComponent<Animator>();
             _cameraTransform = Camera.main.transform;
         }
 
-        void Start() {
-            if (!_characterModelTransform) {
+        void Start()
+        {
+            if (!_characterModelTransform)
+            {
                 throw new MissingFieldException("characterModelTransform is required!");
             }
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        void FixedUpdate() {
-            GroundCheck();
+        void FixedUpdate()
+        {
             Vector2 move = _playerInput.actions["Move"].ReadValue<Vector2>();
 
-            // Animate character
-            _animator.SetFloat("x", move.x);
-            _animator.SetFloat("y", move.y);
+            GroundCheck();
+            Animate(move);
 
-            if (move == Vector2.zero) {
+            if (move == Vector2.zero)
+            {
                 // Stop movement when no input is detected
                 _rigidbody.velocity = new Vector3(0f, _rigidbody.velocity.y, 0f);
                 // If we won't return here then the camera keeps on rotating which will move the character too.
                 return;
             }
             Vector3 movement = new Vector3(move.x, 0f, move.y);
-            
+
             // For rotating the character to the camera facing side when moving forward.
-            movement = movement.x * _cameraTransform.right.normalized + movement.z * _cameraTransform.forward.normalized;
-            movement *= _speed;
+            movement =
+                movement.x * _cameraTransform.right.normalized
+                + movement.z * _cameraTransform.forward.normalized;
+            movement *= _isRunning ? _runSpeed : _walkSpeed;
             movement.y = _rigidbody.velocity.y;
 
             _rigidbody.velocity = movement;
 
-
             // Rotate the character to the forward moving direction.
-            if (movement != Vector3.zero) {
-                movement.y = 0f; 
+            if (movement != Vector3.zero)
+            {
+                movement.y = 0f;
                 Quaternion rotation = Quaternion.LookRotation(movement);
-                _characterModelTransform.rotation = Quaternion.Lerp(_characterModelTransform.rotation, rotation,
-                        _rotationSpeed * Time.fixedDeltaTime);
+                _characterModelTransform.rotation = Quaternion.Lerp(
+                    _characterModelTransform.rotation,
+                    rotation,
+                    _rotationSpeed * Time.fixedDeltaTime
+                );
             }
         }
 
-        public void OnJump() {
-            if (_isGrounded) {
+        // Method that will be called by the unity input system
+        public void OnJump()
+        {
+            if (_isGrounded)
+            {
                 Debug.Log("Jump!");
                 _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             }
         }
 
-        private void GroundCheck() {
-            _isGrounded = Physics.Raycast(transform.position, Vector3.down, _groundCheckDistance, _groundLayer);
+        public void OnRun(InputValue inputValue)
+        {
+            _isRunning = inputValue.isPressed;
+        }
+
+        // Utility methods
+        private void GroundCheck()
+        {
+            _isGrounded = Physics.Raycast(
+                transform.position,
+                Vector3.down,
+                _groundCheckDistance,
+                _groundLayer
+            );
+        }
+
+        private void Animate(Vector2 movement)
+        {
+            // Animation speed shouldn't be inlfluenced by the actual movement speed, So having separate separate multiplier for
+            // animation.
+            float walkAnimSpeed = 2f;
+            float runAnimSpeed = 5f;
+            _animator.SetFloat(
+                "x",
+                _isRunning ? runAnimSpeed * movement.x : walkAnimSpeed * movement.x
+            );
+            _animator.SetFloat(
+                "y",
+                _isRunning ? runAnimSpeed * movement.y : walkAnimSpeed * movement.y
+            );
         }
     }
 }
